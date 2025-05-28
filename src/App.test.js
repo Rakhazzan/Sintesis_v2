b1f5c5f210515d31c454397e41ee52c5236ba859
Rakhazzan <makhazzan@inscastellet.cat>
2025-05-28 02:27:11 +0200

    app.test actualizado

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders login page when not authenticated', () => {
+    render(<App />);
+    const loginTitle = screen.getByText(/iniciar sesión/i);
+    expect(loginTitle).toBeInTheDocument();
+  });
+
+  test('renders registration link', () => {
+    render(<App />);
+    const registerLink = screen.getByText(/regístrate/i);
+    expect(registerLink).toBeInTheDocument();
+  });
 });
